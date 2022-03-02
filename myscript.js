60269a4b288f3b204ffadf8c5a316b561148b62c
shahzad-22 <shahzadwaheed48@gmail.com>
2022-03-02 20:54:00 +0500

    commit

---
+++ myscript.js
@@ -0,0 +1,33 @@
+//Get the screen
+let screen = document.getElementById("screen");
+// Get buttons in js
+
+let buttons = document.querySelectorAll("button");
+let screenValue = "";
+let button // this is for change
+    // for (item of buttons) {
+//   item.addEventListener("click", (e) => {
+//     buttonText = e.target.innerText;
+//     screenValue.appendChild(buttonText);
+
+
+//  }
+
+
+
+
+    // console.log("Button text is ", buttonText);
+    // if (buttonText == "x") {
+    //   console.log("Button text is  x wala button ki output", buttonText);
+    //   buttonText += "*";
+    //   console.log("Button text is * wala button ki out ", buttonText);
+    //   screenValue.append(buttonText) = buttonText;
+     }// else if (buttonText == "RESET") {
+    //   screenValue = "";
+    // } else if (buttonText == "Del") {
+    //   screenValue = "";
+    // } else if (buttonText == "=") {
+    //   screenValue = eval(screenValue);
+    // }
+  });
+}
